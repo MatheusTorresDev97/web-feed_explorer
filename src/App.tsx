ed0287ae0d7bcc2fd29e3768c03c0482a0467d17
Matheus <matheustorresdev@gmail.com>
2023-12-15 20:15:21 -0300

    Criando Header e Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
+import { Header } from "./components/Header";
 import styles from "./App.module.css";
 
 import "./global.css";
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/matheusTorresDev97.png',
+      name: 'Matheus Torres',
+      role: 'Policial Militar'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/matheusTorresDev97.png',
+      name: 'Matheus Torres',
+      role: 'Policial Militar'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
 
 function App() {
   return (
     <div>
       <div className={styles.wrapper}>
+        <Header />
+
+        <div className={styles.wrapper}>
         
+        </div>
       </div>
     </div>
   );

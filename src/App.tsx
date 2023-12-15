3088a1b86a16937fe3839c46435a48750227412b
Matheus <matheustorresdev@gmail.com>
2023-12-15 20:23:01 -0300

    create component Post

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post, PostType } from './components/Post'
 import styles from "./App.module.css";
 
 import "./global.css";
@@ -7,41 +9,59 @@ const posts: PostType[] = [
   {
     id: 1,
     author: {
-      avatarUrl: 'https://github.com/matheusTorresDev97.png',
-      name: 'Matheus Torres',
-      role: 'Policial Militar'
+      avatarUrl: "https://github.com/matheusTorresDev97.png",
+      name: "Matheus Torres",
+      role: "Policial Militar",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2022-05-03 20:00:00'),
+    publishedAt: new Date("2022-05-03 20:00:00"),
   },
   {
     id: 2,
     author: {
-      avatarUrl: 'https://github.com/matheusTorresDev97.png',
-      name: 'Matheus Torres',
-      role: 'Policial Militar'
+      avatarUrl: "https://github.com/matheusTorresDev97.png",
+      name: "Matheus Torres",
+      role: "Policial Militar",
     },
     content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2022-05-10 20:00:00'),
+    publishedAt: new Date("2022-05-10 20:00:00"),
   },
 ];
 
 function App() {
   return (
     <div>
+      <Header />
+      
       <div className={styles.wrapper}>
-        <Header />
-
         <div className={styles.wrapper}>
-        
+          <Sidebar />
+          <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                post={post}
+              />
+            )
+          })}
+          </main>
         </div>
       </div>
     </div>

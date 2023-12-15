fd1b2443103a688b3b1dfc09f927d006895853dc
Matheus <matheustorresdev@gmail.com>
2023-12-15 20:08:53 -0300

    definindo css global

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import styles from "./App.module.css";
+
+import "./global.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div className={styles.wrapper}>
+        
+      </div>
+    </div>
+  );
 }
 
 export default App;
